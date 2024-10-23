3bcfec9f0be390780a358a3b24cc3546fce6bf52
mourad-edine <chamsedinemaulice08@gmail.com>
2024-10-23 20:01:46 +0300

    ajout de nouvelles fonctionnalité

--- app/Http/Controllers/commande/CommandeController.php
+++ app/Http/Controllers/commande/CommandeController.php
@@ -1,100 +1,107 @@
 <?php
 
-namespace App\Http\Controllers\livraison;
+namespace App\Http\Controllers\commande;
 
 use App\Http\Controllers\Controller;
-use App\Models\Commande;
-use App\Models\Dechet;
 use Illuminate\Http\Request;
-use App\Models\Livraison;
-use App\Models\Payement;
+use App\Models\Commande;
 
-class LivraisonController extends Controller
+class CommandeController extends Controller
 {
-    public function store_livraison(Request $request)
+    public function store_commande(Request $request)
     {
-        if ($request) {
+        $valeur_sans_espace = str_replace(' ', '', $request->input('montant_avance'));
+        $montant = (int)$valeur_sans_espace;
+       if ($request){
             $var = [
-                'commande_id' => (int)$request->commande_id,
-                'quantite' => (int)$request->quantite,
-                'nombre_caissette' => (int)$request->nombre_caissette,
-                'date_livraison' => $request->date_livraison,
-                'etat' => 'non payé'
+                'fournisseur_id' => (int)$request->fournisseur_id,
+                'nom_sous_fournisseur' => $request->nom_sous_fournisseur,
+                'evenement_id' => $request->evenement_id,
+                'quantite_commande' => (int)$request->quantite,
+                'etat' => "envoyé",
+                'montant_avance' => (int)$montant,
+                'date_commande' => $request->date
+                //etat ,id_commande
             ];
-
-            $commande = Commande::findOrFail($request->commande_id);
-            if ($commande) {
-                $test = (int)$commande->quantite_livre + (int)$request->quantite;
-                if($test == (int)$commande->quantite_commande){
-                    $commande->etat = "livré";
-                    $commande->quantite_livre = $test;
-                    $commande->save();
-                    $livraison = Livraison::create($var);
-
-                    return response()->json([
-                        'message' => 'commande livré',
-                        'valeur' => $livraison
-
-                    ]);
-
-                }else if($test < (int)$commande->quantite_commande){
-                    $commande->etat = "encours";
-                    $commande->quantite_livre = $test;
-                    $commande->save();
-
-                    $livraison = Livraison::create($var);
-                    return response()->json([
-                        'message' => 'commande encours',
-                        'valeur' => $livraison
-                    ]);
-                    
-                }else if($test > (int)$commande->quantite_commande){
-                    return response()->json([
-                        'valeur' => 'valeur trop grande !'
-                    ]);
-
-                }else{
-                    return response()->json([
-                        'valeur' => 'erreur inatendu de votre part !'
-                    ]);
-                }
-
-            }
+            $insert = Commande::create($var);
+            return response()->json([
+                'message' => 'Success',
+                'valeurs' => $insert
+            ]);
+        
+        
+        }else{
+            $var = [
+                'fournisseur_id' => (int)$request->fournisseur_id,
+                'nom_sous_fournisseur' => $request->nom_sous_fournisseur,
+                'evenement_id' => $request->evenement_id,
+                'quantite_commande' => (int)$request->quantite,
+                'etat' => "envoyé",
+                'montant_avance' => (int)$montant,
+                'date_commande' => $request->date
+                //etat ,id_commande
+            ];
+            $insert = Commande::create($var);
+            return response()->json([
+                'message' => 'Success',
+                'valeur' => $insert
+            ]);
         }
-
     }
-    public function showLivraison()
-    {
-        $test = new Livraison();
-        return $test->getLivraison();
+    ///commande encoours ou envoyé
+    public function showCommande(){
+        $test = new Commande();
+        return $test->getCommande();
     }
 
-    public function detailLivraison($id)
-    {
-        $test = Livraison::findOrfail($id);
+    public function showCommandeNoLivre(){
+        $test = new Commande();
+        return $test->getCommandeNoLivre();
+    }
+///toutes les commandes annulé
+    public function showCommandeAnnule(){
+        $test = new Commande();
+        return $test->getCommandeAnnule();
+    }
+    
+    public function detailCommande($id){
+        $test = Commande::findOrfail($id);
         return $test;
     }
+    public function Show_lastIdCommande(){
+        $lastItem = Commande::orderBy('id', 'desc')->first();
+        return $lastItem;
+    }
 
-    public function deleteLivraison($id)
-    {
-        $valeur = Livraison::findorfail($id);
-        if ($valeur) {
+    public function deleteCommande($id){
+        $valeur = Commande::findorfail($id);
+        if($valeur){
             $valeur->delete();
         }
         return response()->json([
             'message' => 'valeur supprimé avec succès'
         ]);
     }
-
-    public function getPaid()
-    {
-        $valeur = Livraison::where('etat', 'payé')->get();
-        return $valeur;
+    public function detailCommande2($id){
+        $valeur = Commande::findorfail($id);
+        return response()->json([
+            'quantite_commande' => $valeur->quantite_commande
+        ]);
     }
 
-    public function getUnpaid()
-    {
-        $valeur = Livraison::where('etat', 'non payé')->get();
-        return $valeur;
+    public function AnnulerCommande($id){
+        $valeur = Commande::findorfail($id);
+        if($valeur){
+            $valeur->etat = "annulé";
+            $valeur->save();
+        }
+        return response()->json([
+            'message' => 'commande annulé avec succes'
+        ]);
+    }
+    public function showCommandeAll(){
+        return Commande::with('fournisseur')->get();
     }
 }
+
+
