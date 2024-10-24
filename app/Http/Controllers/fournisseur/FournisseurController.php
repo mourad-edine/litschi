8b9c3ed64d8aae9db379760c3ec8bf6f4439dd9d
mourad-edine <chamsedinemaulice08@gmail.com>
2024-10-24 08:07:42 +0300

    ajout de nouvelles fonctionnalité

--- app/Http/Controllers/fournisseur/FournisseurController.php
+++ app/Http/Controllers/fournisseur/FournisseurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\fournisseur;
 
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Models\Fourniseur;
 use Illuminate\Http\Request;
@@ -18,25 +19,34 @@ class FournisseurController extends Controller
             ];
             $insert = Fourniseur::firstOrCreate($var);
             return $insert;
-
         }
     }
 
-    public function showFournisseur(){
+    public function showFournisseur()
+    {
         $test = new Fourniseur();
         return $test->getFournisseur();
     }
 
-    public function detailFournisseur($id){
+    public function detailFournisseur($id)
+    {
         $test = Fourniseur::findOrfail($id);
         return $test;
     }
 
-    public function deleteFournisseur($id){
+    public function deleteFournisseur($id)
+    {
         $valeur = Fourniseur::findorfail($id);
-        if($valeur){
+        $paletteIds = DB::table('palette_fournisseurs')
+            ->where('fournisseur_id', $valeur->id)
+            ->pluck('palette_id');
+        foreach ($paletteIds as $ids) {
+            DB::table('palettes')->where('id', $ids)->delete();
+        }
+        if ($valeur) {
             $valeur->delete();
         }
+
         return response()->json([
             'messages' => 'valeur supprimé avec succès'
         ]);
